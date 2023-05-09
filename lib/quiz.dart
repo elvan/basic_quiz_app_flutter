dae32e873e35e3b00d1440807c6734fc33291453
Elvan Hidayat <elvanhd@gmail.com>
2023-05-09 16:33:45 +0700

    rendering content conditionally

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'questions_screen.dart';
 import 'start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -12,6 +13,20 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -27,7 +42,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
